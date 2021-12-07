fbb55cf7520aba0662570f24e0165f13316ad6de
Chexpeare <cguevara@nj.rr.com>
2021-12-07 11:04:40 -0500

    Clean up code

--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcAccountsDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcAccountsDao.java
@@ -18,12 +18,14 @@ public class JdbcAccountsDao implements AccountsDao {
 
     @Override
     public Account getBalance(int id) {
+
         String sql = "SELECT account_id, users.user_id, balance " +
                 "FROM accounts " +
                 "JOIN users ON users.user_id = accounts.user_id " +
                 "WHERE users.user_id = ?;";
-        Account account = null;
+
         SqlRowSet results = jdbcTemplate.queryForRowSet(sql, id);
+        Account account = null;
         while(results.next()) {
             account = mapRowToAccount(results);
             System.out.println(account);
@@ -31,12 +33,12 @@ public class JdbcAccountsDao implements AccountsDao {
         return account;
     }
 
-    private Account mapRowToAccount(SqlRowSet rs) {
-        Account cc = new Account();
-        cc.setAccountId(rs.getLong("account_id"));
-        cc.setUserId(rs.getLong("user_id"));
-        cc.setBalance(rs.getDouble("balance"));
-        return cc;
+    private Account mapRowToAccount(SqlRowSet results) {
+        Account acctData = new Account();
+        acctData.setAccountId(results.getLong("account_id"));
+        acctData.setUserId(results.getLong("user_id"));
+        acctData.setBalance(results.getDouble("balance"));
+        return acctData;
     }
 
 }
